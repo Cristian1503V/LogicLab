7d0f40a5ab20d87bad3c4e0eaa0228bc42d8ea21
Cristian Vega <crisda.arive.1503@gmail.com>
2023-10-23 08:46:58 -0500

    Add Dropdown File

---
+++ LogicLab/Views/Js/JavaScript.js
@@ -0,0 +1,15 @@
+﻿document.addEventListener("click", (event) => {
+    const dropdowns = document.querySelectorAll(".dropdown");
+    
+    dropdowns.forEach((dropdown) => {
+        const dropdownContent = dropdown.querySelector(".dropdown-content");
+  
+        if (dropdown.contains(event.target)) {
+            // Click is inside the current dropdown, toggle the class
+            dropdownContent.classList.toggle("hidden");
+        } else {
+            // Click is outside the current dropdown, hide it
+            dropdownContent.classList.add("hidden");
+        }
+    });
+});
