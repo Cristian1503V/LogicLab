fc9ba5a77683097ab755cb3a182caae38f2fa66a
Cristian Vega <crisda.arive.1503@gmail.com>
2023-10-23 08:35:39 -0500

    Add EmailForm Entity

---
+++ LogicLab/Entities/EmailFormE.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LogicLab.Entities
+{
+    public class EmailFormE
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+
+    }
+}
