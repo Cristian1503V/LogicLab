292837d5c233f650848e855fafed6ae277da2c71
Cristian Vega <crisda.arive.1503@gmail.com>
2023-10-23 08:33:23 -0500

    Add Glebal File

---
+++ LogicLab/Global.asax.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace LogicLab
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+            Session["userName"] = null;
+            Session["userEmail"] = null;
+            Session["userImage"] = null;
+            Session["userPhoneNumber"] = null;
+            Session["userRole"] = null;
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
