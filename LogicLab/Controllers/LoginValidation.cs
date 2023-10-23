32745a148a743d5838df192305408a6db8bbf15a
Cristian Vega <crisda.arive.1503@gmail.com>
2023-10-23 08:37:27 -0500

    Add LoginValidation Login

---
+++ LogicLab/Controllers/LoginValidation.cs
@@ -0,0 +1,34 @@
+﻿using LogicLab.Data;
+using LogicLab.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LogicLab.Controllers
+{
+    public class LoginValidation
+    {
+        public Boolean MtValidateUser( UserCredential userCredential)
+        {
+           User userFound =  UserData.MTGetUserByEmail(userCredential.Email);
+
+            if (userFound == null ) return false;
+
+            Boolean isUserValid = userCredential.Password == userFound.Password;
+
+            if (!isUserValid) return false;
+
+
+            HttpContext.Current.Session["userName"] = userFound.Name;
+            HttpContext.Current.Session["userEmail"] = userFound.Email;
+            HttpContext.Current.Session["userImage"] = userFound.Image;
+            HttpContext.Current.Session["userPhoneNumber"] = userFound.PhoneNumber;
+            HttpContext.Current.Session["userRole"] = userFound.Role;
+
+
+            return true;
+
+        }
+    }
+}
